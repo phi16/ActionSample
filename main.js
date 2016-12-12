a1033645601306243132352b042a216672ed9abd
phi16 <phi16.256@gmail.com>
2016-12-12 19:49:24 +0900

    Impl : jumping, bounding frame

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 const cw = 50, ch = 100;
 var x = width/2-cw/2, y = height/2, vx = 0, vy = 0;
+var jumping = false;
 
 var key = {left:false,down:false,up:false,right:false};
 
 function execute(draw){
   draw(x,y,cw,ch);
-  if(key.left)vx-=3;
-  if(key.right)vx+=3;
-  vx *= 0.8;
-  vy += 0.4;
+  if(!jumping){
+    if(key.left)vx-=3;
+    if(key.right)vx+=3;
+    vx *= 0.8;
+  }else vx *= 0.99;
+  vy += 0.8;
   x += vx;
   y += vy;
   if(y > height-ch){
     vy = 0;
     y = height-ch;
+    jumping = false;
   }
+  if(key.up && !jumping){
+    vy = -15;
+    jumping = true;
+  }
+  if(x < 0)x = 0, vx = 0;
+  if(x > width-cw)x = width-cw, vx = 0;
 }
 function input(e,pressing){
   if(e.keyCode==37)key.left = pressing;
