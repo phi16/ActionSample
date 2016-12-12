af3a9604b5e7f15ec02b31b862104ea985d7de6b
phi16 <phi16.256@gmail.com>
2016-12-12 23:52:13 +0900

    Separate common codes

--- main.js
+++ main.js
@@ -6,92 +6,62 @@ var key = {left:false,down:false,up:false,right:false};
 
 var current;
 
-function ground(){
-  if(key.left)vx-=3;
-  if(key.right)vx+=3;
-  vx *= 0.8
-  vy += 0.8;
-  x += vx;
-  y += vy;
-  if(y > height-ch){
-    vy = 0;
-    y = height-ch;
+var ground,jump1,jump2,wall;
+function basis(isGround,isWall,jump){
+  return function(){
+    if(key.left)vx -= isGround ? 3 : 0.001;
+    if(key.right)vx += isGround ? 3 : 0.001;
+    vx *= isGround ? 0.8 : 0.99;
+    vy += 0.8;
+    if(isWall){
+      if(x < width/2 && key.left || x > width/2 && key.right){
+        vy = Math.min(vy,3.0);
+      }
+    }
+    x += vx;
+    y += vy;
+    if(y > height-ch){
+      vy = 0;
+      y = height-ch;
+      current = ground;
+    }
+    if(key.up){
+      jump();
+    };
+    if(x < 0 && vx < 0){
+      x = 0, vx = 0;
+      if(!isGround)current = wall;
+    }
+    if(x > width-cw && vx > 0){
+      x = width-cw, vx = 0;
+      if(!isGround)current = wall;
+    }
   }
-  if(key.up){
+}
+
+ground = basis(true,false,function(){
+  vy = -15;
+  current = jump1;
+  jumpable = false;
+});
+jump1 = basis(false,false,function(){
+  if(jumpable){
     vy = -15;
+    current = jump2;
+    jumpable = false;
+  }
+});
+jump2 = basis(false,false,function(){
+});
+wall = basis(false,true,function(){
+  if(jumpable){
+    vy = -15;
+    if(x < width/2)vx = 10;
+    else vx = -10;
     current = jump1;
     jumpable = false;
-  };
-  if(x < 0 && vx < 0)x = 0, vx = 0;
-  if(x > width-cw && vx > 0)x = width-cw, vx = 0;
-}
-
-function jump1(){
-  if(key.left)vx-=0.001;
-  if(key.right)vx+=0.001;
-  vx *= 0.99;
-  vy += 0.8;
-  x += vx;
-  y += vy;
-  if(y > height-ch){
-    vy = 0;
-    y = height-ch;
-    current = ground;
   }
-  if(key.up){
-    if(jumpable){
-      vy = -15;
-      current = jump2;
-      jumpable = false;
-    }
-  }
-  if(x < 0 && vx < 0)x = 0, vx = 0, current = wall;
-  if(x > width-cw && vx > 0)x = width-cw, vx = 0, current = wall;
-}
-
-function jump2(){
-  if(key.left)vx-=0.001;
-  if(key.right)vx+=0.001;
-  vx *= 0.99;
-  vy += 0.8;
-  x += vx;
-  y += vy;
-  if(y > height-ch){
-    vy = 0;
-    y = height-ch;
-    current = ground;
-  }
-  if(x < 0 && vx < 0)x = 0, vx = 0, current = wall;
-  if(x > width-cw && vx > 0)x = width-cw, vx = 0, current = wall;
-}
-
-function wall(){
-  if(key.left)vx-=0.001;
-  if(key.right)vx+=0.001;
-  vx *= 0.99;
-  vy += 0.8;
-  if(x < width/2 && key.left || x > width/2 && key.right){
-    vy = Math.min(vy,3.0);
-  }
-  x += vx;
-  y += vy;
-  if(y > height-ch){
-    vy = 0;
-    y = height-ch;
-    current = ground;
-  }
-  if(key.up){
-    if(jumpable){
-      vy = -15;
-      if(x < width/2)vx = 10;
-      else vx = -10;
-      current = jump1;
-      jumpable = false;
-    }
-  }
-  if(x < 0 && vx < 0)x = 0, vx = 0;
-  if(x > width-cw && vx > 0)x = width-cw, vx = 0;
-}
+});
 
 current = ground;
 
